4bf3cf5beda7f8aac671d7bf80374bfec11f420d
kc2020-ai <kuochiu2020@gmail.com>
2025-06-29 08:01:28 -0700

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -178,7 +178,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("dca017aaf7f98789")
+/******/ 		__webpack_require__.h = () => ("53561658174470aa")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
